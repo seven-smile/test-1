e57a99516a4781c7e9427495f4cfd70019ba600e
Miles <juniornyamile@gmail.com>
2024-08-15 18:32:38 +0000

    resolving connected hosts

--- test1/settings.py
+++ test1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gv1hb)q2glen==*2#&q#)!bbs^k^bd5$1i_icd*be==!i^dt_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
